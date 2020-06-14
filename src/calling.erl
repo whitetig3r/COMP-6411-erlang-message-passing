338f6bbbb966ff2f5a6fd283e343491caa81e05f
Warren White <warren1linux@gmail.com>
2020-06-13 22:12:54 -0400

    add and complete all requirements

--- src/calling.erl
+++ src/calling.erl
@@ -1,13 +1,46 @@
-%%%-------------------------------------------------------------------
-%%% @author warrenwhite
-%%% @copyright (C) 2020, <COMPANY>
+ %%%-------------------------------------------------------------------
+%%% @author Warren White
+%%% @copyright (C) 2020
 %%% @doc
-%%%
+%%% The child processes execute methods in this file.
 %%% @end
 %%% Created : 13. Jun 2020 4:02 AM
 %%%-------------------------------------------------------------------
 -module(calling).
 -author("warrenwhite").
+-export([receive_message/1, send_intro_message/2]).
 
-%% API
--export([]).
+ randomized_sleep() ->
+   random:seed(now()),
+   timer:sleep(random:uniform(100)).
+
+ send_intro_message(CALLER, CALLEES) ->
+   lists:foreach(
+     fun(CALLEE) ->
+       randomized_sleep(),
+       CALLEE ! {CALLER, intro}
+     end,
+     CALLEES
+     ).
+
+ receive_message(CALLER) ->
+   receive
+     {PROCESS_IDENTIFIER, intro} ->
+       INITIAL_CONTACT_TIMESTAMP = element(3,now()),
+       master !
+         [CALLER, intro, PROCESS_IDENTIFIER, INITIAL_CONTACT_TIMESTAMP],
+       randomized_sleep(),
+       PROCESS_IDENTIFIER ! {CALLER, INITIAL_CONTACT_TIMESTAMP, reply},
+       receive_message(CALLER);
+     {PROCESS_IDENTIFIER, INITIAL_CONTACT_TIMESTAMP, reply} ->
+       master !
+         [CALLER, reply, PROCESS_IDENTIFIER, INITIAL_CONTACT_TIMESTAMP],
+       receive_message(CALLER);
+     {CALLEES, start} ->
+       send_intro_message(CALLER, CALLEES),
+       receive_message(CALLER)
+   after
+    5000 ->
+      io:fwrite("Process ~w has received no calls for 5 seconds, ending... ~n",[CALLER]),
+      exit(CALLER)
+   end.
--- src/exchange.erl
+++ src/exchange.erl
@@ -1,19 +1,24 @@
 %%%-------------------------------------------------------------------
-%%% @author warrenwhite
-%%% @copyright (C) 2020, <COMPANY>
+%%% @author Warren White
+%%% @copyright (C) 2020
 %%% @doc
-%%%
+%%% The master process runs the functions in this file.
+%%% It spawns the required child processes.
 %%% @end
 %%% Created : 13. Jun 2020 2:24 AM
 %%%-------------------------------------------------------------------
 -module(exchange).
 -author("warrenwhite").
--export([start/0]).
+-export([start/0, receive_messages_to_print/0]).
 
 start() ->
   io:fwrite("** Calls to be made **~n"),
-  { ok, TUPLES } = file:consult("src/calls.txt"),
-  print_list(TUPLES).
+  { ok, TUPLES } = file:consult("calls.txt"),
+  print_list(TUPLES),
+  register(master, self()),
+  lists:foreach(fun spawn_calling_process/1, TUPLES),
+  [ CALLER ! {CALLEES, start} || {CALLER, CALLEES} <- TUPLES ],
+  receive_messages_to_print().
 
 print_list(List)->
   [io:format("~p: ~p ~n",[tuple_head(X),tuple_tail(X)]) || X <- List].
@@ -25,3 +30,20 @@ tuple_head(List) ->
 tuple_tail(List) ->
   {_, TAIL} = List,
   TAIL.
+
+spawn_calling_process(TUPLE) ->
+  {CALLER, _} = TUPLE,
+  register(CALLER, spawn(calling, receive_message, [CALLER])).
+
+receive_messages_to_print() ->
+  receive
+    [CALLER, intro, PROCESS_IDENTIFIER, T_STAMP] ->
+      io:fwrite("~w received ~w message from ~w (~w)~n",[CALLER, intro, PROCESS_IDENTIFIER, T_STAMP]),
+      receive_messages_to_print();
+    [CALLER, reply, PROCESS_IDENTIFIER, T_STAMP] ->
+      io:fwrite("~w received ~w message from ~w (~w)~n",[CALLER, reply, PROCESS_IDENTIFIER, T_STAMP]),
+      receive_messages_to_print()
+    after
+      10000 ->
+        io:fwrite("Master has received no replies for 10 seconds, ending...")
+  end.
